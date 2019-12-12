93b1634b39bffc8706f7757a9244380fc141b0f8
austin martinez <amartinez21@weldre4.org>
2019-12-12 09:17:04 -0700

    I add  encoders  to the program so we can  stat testing the autonomous and make sure that the vuforia can locate the sky stone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneDetectionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneDetectionTest.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
+
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
@@ -35,11 +37,12 @@ import java.util.List;
 @Autonomous(name= "opencvSkystoneDetector", group="Sky autonomous")
 //@Disabled
 public class SkystoneDetectionTest extends LinearOpMode {
-    public DcMotor BL_drive = null;
-    public DcMotor BR_drive = null;
-    public DcMotor FL_drive = null;
-    public DcMotor FR_drive = null;
+    public DcMotor BL_DRIVE = null;
+    public DcMotor BR_DRIVE = null;
+    public DcMotor FL_DRIVE = null;
+    public DcMotor FR_DRIVE = null;
     private ElapsedTime runtime = new ElapsedTime();
+    private Servo SL_PULL = null;
 
     //0 means skystone, 1 means yellow stone
     //-1 for debug, but we can keep it like this because if it works, it should change to either 0 or 255
@@ -69,10 +72,10 @@ public class SkystoneDetectionTest extends LinearOpMode {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = new OpenCvWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
 
-        BL_drive = hardwareMap.get(DcMotor.class, "BL");
-        BR_drive = hardwareMap.get(DcMotor.class, "BR");
-        FL_drive = hardwareMap.get(DcMotor.class, "FL");
-        FR_drive = hardwareMap.get(DcMotor.class, "FR");
+        BL_DRIVE = hardwareMap.get(DcMotor.class, "BL");
+        BR_DRIVE = hardwareMap.get(DcMotor.class, "BR");
+        FL_DRIVE = hardwareMap.get(DcMotor.class, "FL");
+        FR_DRIVE = hardwareMap.get(DcMotor.class, "FR");
 
 
         webcam.openCameraDevice();//open camera
@@ -100,139 +103,254 @@ public class SkystoneDetectionTest extends LinearOpMode {
 
             if(valLeft == 255 && valMid == 0 && valRight == 0){
                 sleep(200);
-                BR_drive.setPower(1);
-                BL_drive.setPower(1);
+                encoderDrive(11.8,1,100,100,"DOWN");
+                encoderTurn(10,1,100,"RIGHT");
+
+
+            }
+            if(valLeft == 0 && valMid == 255 && valRight == 0){
+                sleep(200);
+                encoderDrive(11,1,100,100,"UP");
+                encoderTurn(10,1,100,"LEFT");
+
+
+            }
+            if(valLeft == 0 && valMid == 0 && valRight == 255 ){
+                sleep(200);
+                encoderDrive(11,1,100,100,"UP");
+                encoderTurn(10,1,100,"RIGHT");
 
             }
 
         }
 
     }
+    public void encoderDrive(double Inches, double Speed, int SleepTimeA, int SleepTimeB, String ServoPos){
 
-    //detection pipeline
-    static class StageSwitchingPipeline extends OpenCvPipeline {
-        Mat yCbCrChan2Mat = new Mat();
-        Mat thresholdMat = new Mat();
-        Mat all = new Mat();
-        List<MatOfPoint> contoursList = new ArrayList<>();
+        double Diameter = 11.21;
+        double EncoderTurns = 288;
+        double DesiredPos = Inches * EncoderTurns / Diameter;
 
-        enum Stage {//color difference. greyscale
-            detection,//includes outlines
-            THRESHOLD,//b&w
-            RAW_IMAGE,//displays raw view
+        BL_DRIVE.setTargetPosition((int) DesiredPos);
+        BR_DRIVE.setTargetPosition((int) DesiredPos);
+        FL_DRIVE.setTargetPosition((int) DesiredPos);
+        FR_DRIVE.setTargetPosition((int) DesiredPos);
+
+        BL_DRIVE.setPower(Speed);
+        BR_DRIVE.setPower(Speed);
+        FL_DRIVE.setPower(Speed);
+        FR_DRIVE.setPower(Speed);
+
+        BL_DRIVE.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BR_DRIVE.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FL_DRIVE.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FR_DRIVE.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            /*TODO
+               Get servo position and return out of function when at "DOWN" pos rather than using 2 sleep times
+               Tensor flow can see the location of the skystone given that we create a range for the skystone.left value;
+               */
+
+        sleep(SleepTimeA);
+        servoPos(ServoPos, 0);
+
+
+
+        sleep(SleepTimeB);
+
+        resetEncoders();
+    }
+    public void encoderTurn(double Inches, double Speed, int SleepTime, String Direction){
+
+
+        //12in = 90 degrees
+
+        double Diameter = 11.21;
+        double EncoderTurns = 288;
+        double DesiredPos = Inches * EncoderTurns / Diameter;
+
+        if (Direction == "RIGHT") {
+            BL_DRIVE.setTargetPosition((int) DesiredPos);
+            BR_DRIVE.setTargetPosition((int) -DesiredPos);
+            FL_DRIVE.setTargetPosition((int) DesiredPos);
+            FR_DRIVE.setTargetPosition((int) -DesiredPos);
+        }
+        else if (Direction == "LEFT") {
+            BL_DRIVE.setTargetPosition((int) -DesiredPos);
+            BR_DRIVE.setTargetPosition((int) DesiredPos);
+            FL_DRIVE.setTargetPosition((int) -DesiredPos);
+            FR_DRIVE.setTargetPosition((int) DesiredPos);
+        }
+        else {
+            BL_DRIVE.setTargetPosition(0);
+            BR_DRIVE.setTargetPosition(0);
+            FL_DRIVE.setTargetPosition(0);
+            FR_DRIVE.setTargetPosition(0);
         }
 
-        private Stage stageToRenderToViewport = Stage.detection;
-        private Stage[] stages = Stage.values();
+        BL_DRIVE.setPower(Speed);
+        BR_DRIVE.setPower(Speed);
+        FL_DRIVE.setPower(Speed);
+        FR_DRIVE.setPower(Speed);
 
-        @Override
-        public void onViewportTapped() {
-            /*
-             * Note that this method is invoked from the UI thread
-             * so whatever we do here, we must do quickly.
-             */
+        BL_DRIVE.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BR_DRIVE.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FL_DRIVE.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FR_DRIVE.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-            int currentStageNum = stageToRenderToViewport.ordinal();
+        sleep(SleepTime);
 
-            int nextStageNum = currentStageNum + 1;
+        resetEncoders();
+    }
+    public void resetEncoders() {
+        BL_DRIVE.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BR_DRIVE.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FL_DRIVE.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FR_DRIVE.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+    public void servoPos(String position, int SleepTime){
 
-            if (nextStageNum >= stages.length) {
-                nextStageNum = 0;
+        sleep(SleepTime);
+
+        if (position == "UP"){
+            SL_PULL.setPosition(180);
+        }
+        else if (position == "DOWN"){
+            SL_PULL.setPosition(0);
+        }
+        else if (position == "MIDDLE"){
+            SL_PULL.setPosition(90);
+        }
+        else {
+            SL_PULL.setPosition(0);
+        }
+    }
+
+
+        //detection pipeline
+        static class StageSwitchingPipeline extends OpenCvPipeline {
+            Mat yCbCrChan2Mat = new Mat();
+            Mat thresholdMat = new Mat();
+            Mat all = new Mat();
+            List<MatOfPoint> contoursList = new ArrayList<>();
+
+            enum Stage {//color difference. greyscale
+                detection,//includes outlines
+                THRESHOLD,//b&w
+                RAW_IMAGE,//displays raw view
             }
 
-            stageToRenderToViewport = stages[nextStageNum];
-        }
+            private Stage stageToRenderToViewport = Stage.detection;
+            private Stage[] stages = Stage.values();
 
-        @Override
-        public Mat processFrame(Mat input) {
-            contoursList.clear();
-            /*
-             * This pipeline finds the contours of yellow blobs such as the Gold Mineral
-             * from the Rover Ruckus game.
-             */
+            @Override
+            public void onViewportTapped() {
+                /*
+                 * Note that this method is invoked from the UI thread
+                 * so whatever we do here, we must do quickly.
+                 */
 
-            //color diff cb.
-            //lower cb = more blue = skystone = white
-            //higher cb = less blue = yellow stone = grey
-            Imgproc.cvtColor(input, yCbCrChan2Mat, Imgproc.COLOR_RGB2YCrCb);//converts rgb to ycrcb
-            Core.extractChannel(yCbCrChan2Mat, yCbCrChan2Mat, 2);//takes cb difference and stores
+                int currentStageNum = stageToRenderToViewport.ordinal();
 
-            //b&w
-            Imgproc.threshold(yCbCrChan2Mat, thresholdMat, 102, 255, Imgproc.THRESH_BINARY_INV);
+                int nextStageNum = currentStageNum + 1;
 
-            //outline/contour
-            Imgproc.findContours(thresholdMat, contoursList, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
-            yCbCrChan2Mat.copyTo(all);//copies mat object
-            //Imgproc.drawContours(all, contoursList, -1, new Scalar(255, 0, 0), 3, 8);//draws blue contours
-
-
-            //get values from frame
-            double[] pixMid = thresholdMat.get((int) (input.rows() * midPos[1]), (int) (input.cols() * midPos[0]));//gets value at circle
-            valMid = (int) pixMid[0];
-
-            double[] pixLeft = thresholdMat.get((int) (input.rows() * leftPos[1]), (int) (input.cols() * leftPos[0]));//gets value at circle
-            valLeft = (int) pixLeft[0];
-
-            double[] pixRight = thresholdMat.get((int) (input.rows() * rightPos[1]), (int) (input.cols() * rightPos[0]));//gets value at circle
-            valRight = (int) pixRight[0];
-
-            //create three points
-            Point pointMid = new Point((int) (input.cols() * midPos[0]), (int) (input.rows() * midPos[1]));
-            Point pointLeft = new Point((int) (input.cols() * leftPos[0]), (int) (input.rows() * leftPos[1]));
-            Point pointRight = new Point((int) (input.cols() * rightPos[0]), (int) (input.rows() * rightPos[1]));
-
-            //draw circles on those points
-            Imgproc.circle(all, pointMid, 5, new Scalar(255, 0, 0), 1);//draws circle
-            Imgproc.circle(all, pointLeft, 5, new Scalar(255, 0, 0), 1);//draws circle
-            Imgproc.circle(all, pointRight, 5, new Scalar(255, 0, 0), 1);//draws circle
-
-            //draw 3 rectangles
-            Imgproc.rectangle(//1-3
-                    all,
-                    new Point(
-                            input.cols() * (leftPos[0] - rectWidth / 2),
-                            input.rows() * (leftPos[1] - rectHeight / 2)),
-                    new Point(
-                            input.cols() * (leftPos[0] + rectWidth / 2),
-                            input.rows() * (leftPos[1] + rectHeight / 2)),
-                    new Scalar(0, 255, 0), 3);
-            Imgproc.rectangle(//3-5
-                    all,
-                    new Point(
-                            input.cols() * (midPos[0] - rectWidth / 2),
-                            input.rows() * (midPos[1] - rectHeight / 2)),
-                    new Point(
-                            input.cols() * (midPos[0] + rectWidth / 2),
-                            input.rows() * (midPos[1] + rectHeight / 2)),
-                    new Scalar(0, 255, 0), 3);
-            Imgproc.rectangle(//5-7
-                    all,
-                    new Point(
-                            input.cols() * (rightPos[0] - rectWidth / 2),
-                            input.rows() * (rightPos[1] - rectHeight / 2)),
-                    new Point(
-                            input.cols() * (rightPos[0] + rectWidth / 2),
-                            input.rows() * (rightPos[1] + rectHeight / 2)),
-                    new Scalar(0, 255, 0), 3);
-
-            switch (stageToRenderToViewport) {
-                case THRESHOLD: {
-                    return thresholdMat;
+                if (nextStageNum >= stages.length) {
+                    nextStageNum = 0;
                 }
 
-                case detection: {
-                    return all;
-                }
+                stageToRenderToViewport = stages[nextStageNum];
+            }
 
-                case RAW_IMAGE: {
-                    return input;
-                }
+            @Override
+            public Mat processFrame(Mat input) {
+                contoursList.clear();
+                /*
+                 * This pipeline finds the contours of yellow blobs such as the Gold Mineral
+                 * from the Rover Ruckus game.
+                 */
 
-                default: {
-                    return input;
-                }
+                //color diff cb.
+                //lower cb = more blue = skystone = white
+                //higher cb = less blue = yellow stone = grey
+                Imgproc.cvtColor(input, yCbCrChan2Mat, Imgproc.COLOR_RGB2YCrCb);//converts rgb to ycrcb
+                Core.extractChannel(yCbCrChan2Mat, yCbCrChan2Mat, 2);//takes cb difference and stores
 
+                //b&w
+                Imgproc.threshold(yCbCrChan2Mat, thresholdMat, 102, 255, Imgproc.THRESH_BINARY_INV);
+
+                //outline/contour
+                Imgproc.findContours(thresholdMat, contoursList, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+                yCbCrChan2Mat.copyTo(all);//copies mat object
+                //Imgproc.drawContours(all, contoursList, -1, new Scalar(255, 0, 0), 3, 8);//draws blue contours
+
+
+                //get values from frame
+                double[] pixMid = thresholdMat.get((int) (input.rows() * midPos[1]), (int) (input.cols() * midPos[0]));//gets value at circle
+                valMid = (int) pixMid[0];
+
+                double[] pixLeft = thresholdMat.get((int) (input.rows() * leftPos[1]), (int) (input.cols() * leftPos[0]));//gets value at circle
+                valLeft = (int) pixLeft[0];
+
+                double[] pixRight = thresholdMat.get((int) (input.rows() * rightPos[1]), (int) (input.cols() * rightPos[0]));//gets value at circle
+                valRight = (int) pixRight[0];
+
+                //create three points
+                Point pointMid = new Point((int) (input.cols() * midPos[0]), (int) (input.rows() * midPos[1]));
+                Point pointLeft = new Point((int) (input.cols() * leftPos[0]), (int) (input.rows() * leftPos[1]));
+                Point pointRight = new Point((int) (input.cols() * rightPos[0]), (int) (input.rows() * rightPos[1]));
+
+                //draw circles on those points
+                Imgproc.circle(all, pointMid, 5, new Scalar(255, 0, 0), 1);//draws circle
+                Imgproc.circle(all, pointLeft, 5, new Scalar(255, 0, 0), 1);//draws circle
+                Imgproc.circle(all, pointRight, 5, new Scalar(255, 0, 0), 1);//draws circle
+
+                //draw 3 rectangles
+                Imgproc.rectangle(//1-3
+                        all,
+                        new Point(
+                                input.cols() * (leftPos[0] - rectWidth / 2),
+                                input.rows() * (leftPos[1] - rectHeight / 2)),
+                        new Point(
+                                input.cols() * (leftPos[0] + rectWidth / 2),
+                                input.rows() * (leftPos[1] + rectHeight / 2)),
+                        new Scalar(0, 255, 0), 3);
+                Imgproc.rectangle(//3-5
+                        all,
+                        new Point(
+                                input.cols() * (midPos[0] - rectWidth / 2),
+                                input.rows() * (midPos[1] - rectHeight / 2)),
+                        new Point(
+                                input.cols() * (midPos[0] + rectWidth / 2),
+                                input.rows() * (midPos[1] + rectHeight / 2)),
+                        new Scalar(0, 255, 0), 3);
+                Imgproc.rectangle(//5-7
+                        all,
+                        new Point(
+                                input.cols() * (rightPos[0] - rectWidth / 2),
+                                input.rows() * (rightPos[1] - rectHeight / 2)),
+                        new Point(
+                                input.cols() * (rightPos[0] + rectWidth / 2),
+                                input.rows() * (rightPos[1] + rectHeight / 2)),
+                        new Scalar(0, 255, 0), 3);
+
+                switch (stageToRenderToViewport) {
+                    case THRESHOLD: {
+                        return thresholdMat;
+                    }
+
+                    case detection: {
+                        return all;
+                    }
+
+                    case RAW_IMAGE: {
+                        return input;
+                    }
+
+                    default: {
+                        return input;
+                    }
+
+                }
             }
         }
     }
-}
